dc782c4927714b7a91db421492c2fb1c2480ea2c
dverves <denver1033@gmail.com>
2023-11-18 17:57:40 +0200

    first commit

---
+++ cmd/web/routes.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/Pomog/ForumFFF/pkg/config"
+	"github.com/Pomog/ForumFFF/pkg/handler"
+)
+
+func rounts(a *config.AppConfig) http.Handler {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handler.Repo.MainHandler)
+	mux.HandleFunc("/about", handler.Repo.AboutHandler)
+	return mux
+}
