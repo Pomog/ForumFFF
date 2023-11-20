330a36c093264218240d14c6e76728d73dd1fb1c
Pomog <thoryur@gmail.com>
2023-11-20 21:35:36 +0200

    corrections links CSS Images

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -8,7 +8,14 @@ import (
 )
 
 func routes(a *config.AppConfig) http.Handler {
+
 	mux := http.NewServeMux()
+
+	//connect CSS and JS files
+	mux.Handle("/static/css/", http.StripPrefix("/static/css/", http.FileServer(http.Dir("static/css"))))
+	mux.Handle("/static/logo/", http.StripPrefix("/static/logo/", http.FileServer(http.Dir("static/logo"))))
+	mux.Handle("/static/ava/", http.StripPrefix("/static/ava/", http.FileServer(http.Dir("static/ava"))))
+
 	mux.HandleFunc("/", handler.Repo.MainHandler)
 	mux.HandleFunc("/about", handler.Repo.AboutHandler)
 	mux.HandleFunc("/theme", handler.Repo.ThemeHandler)
