a17cf2a293942bd3ab8aeafdb46fadc6def9fbf2
dverves <denver1033@gmail.com>
2023-11-21 07:25:29 +0200

    need to fix pop up window

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/Pomog/ForumFFF/pkg/config"
@@ -11,10 +12,16 @@ func routes(a *config.AppConfig) http.Handler {
 
 	mux := http.NewServeMux()
 
+	statics := []string{"css", "logo", "ava"}
+
+	for _, static := range statics {
+		mux.Handle(fmt.Sprintf("/static/%s/", static), http.StripPrefix(fmt.Sprintf("/static/%s/", static), http.FileServer(http.Dir(fmt.Sprintf("static/%s", static)))))
+	}
+
 	//connect CSS and JS files
-	mux.Handle("/static/css/", http.StripPrefix("/static/css/", http.FileServer(http.Dir("static/css"))))
-	mux.Handle("/static/logo/", http.StripPrefix("/static/logo/", http.FileServer(http.Dir("static/logo"))))
-	mux.Handle("/static/ava/", http.StripPrefix("/static/ava/", http.FileServer(http.Dir("static/ava"))))
+	// mux.Handle("/static/css/", http.StripPrefix("/static/css/", http.FileServer(http.Dir("static/css"))))
+	// mux.Handle("/static/logo/", http.StripPrefix("/static/logo/", http.FileServer(http.Dir("static/logo"))))
+	// mux.Handle("/static/ava/", http.StripPrefix("/static/ava/", http.FileServer(http.Dir("static/ava"))))
 
 	mux.HandleFunc("/", handler.Repo.MainHandler)
 	mux.HandleFunc("/about", handler.Repo.AboutHandler)
