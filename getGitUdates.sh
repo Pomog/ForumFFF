9c76abc118315dabdd20696755c09ce01b533aab
Pomog <thoryur@gmail.com>
2023-11-27 23:01:07 +0200

    getGitUdates.sh

---
+++ getGitUdates.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Use chmod +x getGitUpdates.sh
+
+echo "Getting Updates from GitHub"
+
+# Switch to the main branch
+if git checkout main; then
+    echo "Switched to main branch successfully"
+else
+    echo "Error: Failed to switch to main branch"
+    exit 1
+fi
+
+# Pull the latest changes from the remote main branch
+if git pull origin main; then
+    echo "Pulled changes from main successfully"
+else
+    echo "Error: Failed to pull changes from main"
+    exit 1
+fi
+
+# Switch back to your branch (yurii_dev)
+if git checkout yurii_dev; then
+    echo "Switched back to yurii_dev branch successfully"
+else
+    echo "Error: Failed to switch back to yurii_dev branch"
+    exit 1
+fi
+
+# Rebase your branch onto the latest changes from the local main branch
+if git rebase main; then
+    echo "Rebased yurii_dev onto main successfully"
+else
+    echo "Error: Failed to rebase yurii_dev onto main"
+    exit 1
+fi
+
+echo "Updated Successfully"
