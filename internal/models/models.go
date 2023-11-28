c4333ff5713589f38a8b905039df4cb9c9c76a6e
dverves <denver1033@gmail.com>
2023-11-29 00:22:42 +0200

    working on handlers.go form

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,40 +1,40 @@
 package models
 
-import "time"
-
-
+import (
+	"time"
+)
 
 type User struct {
-	ID int
-	UserName string
-	Password string
-	FirstName string
-	LastName string
-	Email string
-	Created time.Time
-	Picture string
+	ID           int
+	UserName     string
+	Password     string
+	FirstName    string
+	LastName     string
+	Email        string
+	Created      time.Time
+	Picture      string
 	LastActivity time.Time
 }
 
-type Thread struct{
-	ID int
+type Thread struct {
+	ID      int
 	Subject string
 	Created time.Time
-	UserID int
+	UserID  int
 }
 
-type Post struct{
-	ID int
-	Subject string
-	Content string
-	Created time.Time
+type Post struct {
+	ID       int
+	Subject  string
+	Content  string
+	Created  time.Time
 	ThreadId int
-	UserID int
+	UserID   int
 }
 
-type Votes struct{
-	ID int
-	UpCount int
+type Votes struct {
+	ID        int
+	UpCount   int
 	DownCount int
-	PostId int
+	PostId    int
 }
