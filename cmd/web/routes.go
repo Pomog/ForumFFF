e69c428d9b4e8957c288b35da333c278aeae59fe
dverves <denver1033@gmail.com>
2023-11-29 23:43:37 +0200

    final delition of comments

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -18,11 +18,6 @@ func routes(a *config.AppConfig) http.Handler {
 		mux.Handle(fmt.Sprintf("/static/%s/", static), http.StripPrefix(fmt.Sprintf("/static/%s/", static), http.FileServer(http.Dir(fmt.Sprintf("static/%s", static)))))
 	}
 
-	//connect CSS and JS files
-	// mux.Handle("/static/css/", http.StripPrefix("/static/css/", http.FileServer(http.Dir("static/css"))))
-	// mux.Handle("/static/logo/", http.StripPrefix("/static/logo/", http.FileServer(http.Dir("static/logo"))))
-	// mux.Handle("/static/ava/", http.StripPrefix("/static/ava/", http.FileServer(http.Dir("static/ava"))))
-
 	mux.HandleFunc("/", handler.Repo.MainHandler)
 	mux.HandleFunc("/theme", handler.Repo.ThemeHandler)
 	return mux
