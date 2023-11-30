65a29278716ef93edb2887a721743fd1b4a88eeb
dverves <denver1033@gmail.com>
2023-11-30 22:30:39 +0200

    added text file for uuid

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -28,9 +28,7 @@ func main() {
 
 	sessionID = uuid.New()
 
-	app.UserID = &sessionID
-
-	
+	app.UserLogin = &sessionID
 
 	err := run()
 	if err != nil {
@@ -73,31 +71,30 @@ func run() error {
 	return nil
 }
 
-
 // db_driver.MakeDBTables()
 
-	// newUser := models.User{
-	// 	UserName:  "test1",
-	// 	Password:  "123",
-	// 	FirstName: "testFirstName1",
-	// 	LastName:  "testLastName1",
-	// 	Email:     "test1@mail.com",
-	// }
+// newUser := models.User{
+// 	UserName:  "test1",
+// 	Password:  "123",
+// 	FirstName: "testFirstName1",
+// 	LastName:  "testLastName1",
+// 	Email:     "test1@mail.com",
+// }
 
-	// db, _ := db_driver.GetDB()
+// db, _ := db_driver.GetDB()
 
-	// repo := dbrepo.NewSQLiteRepo(db, &app)
+// repo := dbrepo.NewSQLiteRepo(db, &app)
 
-	// repo.CreatetUser(newUser)
+// repo.CreatetUser(newUser)
 
-	// userNameToFind := "test1"
-	// userNameToFind2 := "noSuchUser"
-	// userEmailToFind := "test1@mail.com"
+// userNameToFind := "test1"
+// userNameToFind2 := "noSuchUser"
+// userEmailToFind := "test1@mail.com"
 
-	// isPresent, _ := repo.UserPresent(userNameToFind, userEmailToFind)
-	// log.Printf("User %s present - %v", userNameToFind, isPresent)
-	// fmt.Println("--------------------------------------------------")
-	// isPresent2, _ := repo.UserPresent(userNameToFind2, userEmailToFind)
-	// log.Printf("User %s present - %v", userNameToFind2, isPresent2)
+// isPresent, _ := repo.UserPresent(userNameToFind, userEmailToFind)
+// log.Printf("User %s present - %v", userNameToFind, isPresent)
+// fmt.Println("--------------------------------------------------")
+// isPresent2, _ := repo.UserPresent(userNameToFind2, userEmailToFind)
+// log.Printf("User %s present - %v", userNameToFind2, isPresent2)
 
-	// -------------------------------------------------------------
+// -------------------------------------------------------------
