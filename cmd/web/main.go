edb202637423bf693f09b7f4345ad9fffbe47945
Pomog <thoryur@gmail.com>
2023-12-01 20:38:17 +0200

    test rebase to den_dev

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -70,31 +70,3 @@ func run() error {
 
 	return nil
 }
-
-// db_driver.MakeDBTables()
-
-// newUser := models.User{
-// 	UserName:  "test1",
-// 	Password:  "123",
-// 	FirstName: "testFirstName1",
-// 	LastName:  "testLastName1",
-// 	Email:     "test1@mail.com",
-// }
-
-// db, _ := db_driver.GetDB()
-
-// repo := dbrepo.NewSQLiteRepo(db, &app)
-
-// repo.CreatetUser(newUser)
-
-// userNameToFind := "test1"
-// userNameToFind2 := "noSuchUser"
-// userEmailToFind := "test1@mail.com"
-
-// isPresent, _ := repo.UserPresent(userNameToFind, userEmailToFind)
-// log.Printf("User %s present - %v", userNameToFind, isPresent)
-// fmt.Println("--------------------------------------------------")
-// isPresent2, _ := repo.UserPresent(userNameToFind2, userEmailToFind)
-// log.Printf("User %s present - %v", userNameToFind2, isPresent2)
-
-// -------------------------------------------------------------
