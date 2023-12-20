8c2d181565be98c45d409668e01cd34cb6ed9f79
Pomog <thoryur@gmail.com>
2023-12-20 23:37:47 +0200

    guest and thread corrected in the db

--- internal/repository/dbrepo/sqllite.go
+++ internal/repository/dbrepo/sqllite.go
@@ -2,6 +2,7 @@ package dbrepo
 
 import (
 	"context"
+	"errors"
 	"time"
 
 	"github.com/Pomog/ForumFFF/internal/models"
@@ -134,6 +135,13 @@ func (m *SqliteBDRepo) CreateUser(r models.User) error {
 func (m *SqliteBDRepo) CreateThread(thread models.Thread) (int64, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
+
+	user, _ := m.GetUserByID(thread.UserID)
+	userName := user.UserName
+
+	if userName == "guest" {
+		return 0, errors.New("guest can not create a thread")
+	}
 
 	stmt := `insert into thread
 	(subject, userID)
